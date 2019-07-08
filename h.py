5b1abe6d97cee400df9d302e0ef0d8239eaa79ef
pragati0416 <sharmaanilkumar684@gmail.com>
2019-07-08 14:38:53 +0530

    first commit

---
+++ h.py
@@ -0,0 +1,14 @@
+n1=input("Enter the first value: ")
+n2=input("Enter the second value: ")
+n3=input("Enter the third value:  ")
+ll=[]
+l=[]
+for i in range(n1+1):
+    for j in range(n2+1):
+	    for k in range(n3+1):
+		    l.append(i)
+			l.append(j)
+			l.append(k)
+		l1.append(l)
+        l=[] 
+print(l1)		
